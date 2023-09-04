d4859294f60768bf247e1fb30e0b47b24bb179dc
Kaelan Smith <kaelancsmith@gmail.com>
2023-09-04 14:55:52 -0700

    instantiate new BlockTransformer instead of using static methods

--- includes/API/Widgets.php
+++ includes/API/Widgets.php
@@ -25,6 +25,7 @@ namespace CloakWP\API;
 
 class Widgets
 {
+  private $block_transformer;
 
   public function __construct()
   {
@@ -124,7 +125,7 @@ class Widgets
 
     $instance = $this->get_widget( $object );
 
-    return BlockTransformer::get_blocks( $instance['content'] );
+    return $this->block_transformer->get_blocks( $instance['content'] );
   }
 
 
@@ -137,6 +138,7 @@ class Widgets
    */
   private function bootstrap()
   {
+    $this->block_transformer = new BlockTransformer();
     add_action('rest_api_init', array($this, 'add_blocks_to_widget_rest_responses'));
   }
 }
