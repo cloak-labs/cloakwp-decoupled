caf6084a22a54aba3afd795547e2075afadf874a
Kaelan Smith <kaelancsmith@gmail.com>
2023-10-06 12:08:05 -0700

    Add new Script and Stylesheet classes which extend Asset

---
+++ includes/Admin/Enqueue/Stylesheet.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CloakWP\Admin\Enqueue;
+
+/**
+ * A better API for enqueueing custom CSS files.
+ */
+class Stylesheet extends Asset
+{
+  protected string $enqueueFunction = 'wp_enqueue_style';
+  
+  /**
+   * The media for which this stylesheet has been defined. Accepts media types like 'all' (default),
+   * 'print' and 'screen', or media queries like '(orientation: portrait)' and '(max-width: 640px)'.
+   */
+  public function media(string $media): static
+  {
+    $this->settings['media'] = $media;
+    return $this;
+  }
+}
